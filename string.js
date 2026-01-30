806d2e17a211d2f0a74b70920b54da1b0e4f532b
Rana Bhai <sarifulislam536@gmail.com>
2026-01-31 03:14:26 +0600

    first commit

---
+++ string.js
@@ -0,0 +1,9 @@
+const country="bd"
+console.log(typeof country)
+
+/* string is immutable---->not changeable
+array is mutable ---->we can change the array elements */
+
+const capital="dha ka";
+console.log(capital.length);
+console.log(capital[2]);
