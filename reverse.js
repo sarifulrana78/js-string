98ff8c362ba13b2849e712f94ea8fa62a1ab89ef
Rana Bhai <sarifulislam536@gmail.com>
2026-01-31 04:02:26 +0600

    Reverse a String in Three different ways

---
+++ reverse.js
@@ -0,0 +1,12 @@
+const sen="I Am Learning Web Dev."
+let reverse=""
+for (const letter of sen){
+    /* console.log(letter) */
+    reverse=letter + reverse;
+}
+console.log(reverse)
+
+for (let i= 0;i<sen.length;i++){
+    console.log(i)
+    console.log(sen[i])
+}
