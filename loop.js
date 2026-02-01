5f90b63f035b6d56ab053a11655cf70f62c75797
Rana Bhai <sarifulislam536@gmail.com>
2026-02-01 22:28:43 +0600

    Loop an object and Ways to declare an Object

---
+++ loop.js
@@ -0,0 +1,18 @@
+const mobile ={
+    brand : "samsung",
+    color:"black",
+    price:"25000",
+    camera:"12mp"
+}
+
+/* for of :array
+for in:object */
+for (const prop in mobile){
+    /* console.log(prop)
+    console.log(mobile[prop]) */
+}
+const keys =Object.keys(mobile)
+console.log(keys);
+for (const key of keys ){
+    console.log(key,":",mobile[key])
+}
