d6ce68f1db970b040ac80f6518cda92b8951aa69
Rana Bhai <sarifulislam536@gmail.com>
2026-02-01 21:59:03 +0600

    Multiple ways to get & set object properties

---
+++ property.js
@@ -0,0 +1,21 @@
+const person ={
+    name:"messi",
+    age:25,
+    profession: "dev",
+    salary:20000000,
+    married: true,
+    "fav places": ["bandarban"]
+
+}
+/* console.log(person) */
+/* dot notation */
+console.log(person.name)
+const income=person.salary;
+console.log(income)
+
+
+/* bracket notation */
+
+console.log(person["fav places"])
+
+
---
+++ value .js
@@ -0,0 +1,9 @@
+const person ={
+    name:"messi",
+    age:25,
+    profession: "dev",
+    salary:20000000,
+    married: true,
+    "fav places": ["bandarban", "dhaka"]
+}
+console.log(person)
