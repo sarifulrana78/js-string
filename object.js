90625a66cfc75bb1348b796a2946d2ef1831dc48
Rana Bhai <sarifulislam536@gmail.com>
2026-02-01 21:35:11 +0600

    Introduction to Objects properties and values

--- object.js
+++ object.js
@@ -1,5 +1,21 @@
+/* primitive-primary data ,not changable */
 const age =21;
 const school ="ssc";
 const isPassed= true;
 let isDev;
 console.log(isDev);
+
+/* object declare */
+const bottle ={
+    brand :"apple",
+    price :45,
+    color :white,
+    icClean :false,
+ }
+ /* non-primitive */
+ const subject ={
+    name: "biology",
+    teacher :"ariana",
+    examDate:" 5 feb",
+    chapter :[ "first","second"]
+ }
